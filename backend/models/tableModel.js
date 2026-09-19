55b76f2d3460a03e01b3e428562ded2e3b32c767
tks <t.sauntoo@alustudent.com>
2026-09-19 22:21:06 +0400

    Added table model (restauant table)

---
+++ backend/models/tableModel.js
@@ -0,0 +1,41 @@
+const db = require("../config/database");
+
+const resTable = {
+
+    //getting all tables (restaurant tables)
+    getAll: (callback) => {
+        //prepareing sql
+        const sql = "SELECT * FROM restaurant_tables";
+        //executing the sql query
+        db.query(sql, callback);
+    },
+
+    getById: (id, callback) => {
+
+        const sql = "SELECT * FROM restaurant_tables where table_id = ?";
+        db.query(sql, [id], callback);
+
+    },
+
+    create: (resTable, callback) => {
+
+        const sql = "INSERT INTO restaurant_tables (table_number, capacity, status) VALUES (?,?,?) ";
+        db.query(sql, [resTable.table_number, resTable.capacity, resTable.status], callback );
+
+    },
+
+    update: (id, resTable ,callback) => { 
+
+        const sql = "UPDATE resTable SET table_number = ?, capacity = ?, status = ? WHERE table_id = ?";
+        db.query(sql, [id, resTable.table_number, resTable.capacity, resTable.status], callback);
+
+    },
+
+    delete: (id, callback) => {
+    const sql = "DELETE FROM resTable WHERE table_id = ?";
+    db.query(sql, [id], callback);
+    }
+
+}
+
+module.exports = resTable;
